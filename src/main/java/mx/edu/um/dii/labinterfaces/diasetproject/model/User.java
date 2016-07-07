41f589df0c071288e85394b25b4de378d5ff12ed
Eder Martinez Lap UM Interfaces <edermtz.04@gmail.com>
2016-07-07 18:52:28 -0500

    Add User model

---
+++ src/main/java/mx/edu/um/dii/labinterfaces/diasetproject/model/User.java
@@ -0,0 +1,302 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.um.dii.labinterfaces.diasetproject.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ *
+ * @author laboratoriointerface
+ */
+@Entity
+@Table(name = "users")
+public class User implements Serializable, UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Version
+    private Integer version;
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String username;
+    private String password;
+    private Boolean enabled;
+    private Boolean accountExpired = false;
+    private Boolean accountLocked = false;
+    private Boolean credentialsExpired = false;
+    //Diaset data-Start
+    private String name;
+    private String lastName;
+    private String mothersMaidenName;
+    @Email
+    private String email;
+    //Diaset data-End
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles", joinColumns = {
+        @JoinColumn(name = "user_id")}, inverseJoinColumns
+            = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    //default constructor
+    public User() {
+    }
+
+    //particula constructor
+    public User(String username, String password, String name, String lastName, String mothersMaidenName) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.lastName = lastName;
+        this.mothersMaidenName = mothersMaidenName;
+    }
+
+    /**
+     * @return the Id
+     */
+    public Long getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return the version
+     */
+    public Integer getVersion() {
+        return version;
+    }
+
+    /**
+     * @param version the version to set
+     */
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    /**
+     * @return the username
+     */
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the encrypted password
+     */
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return if the account is enabled
+     */
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * @param enabled the enabled to set
+     */
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /**
+     * @return if the account is non Expired
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return !accountExpired;
+    }
+
+    /**
+     * @param accountExpired the accountExpired to set
+     */
+    public void setAccountExpired(Boolean accountExpired) {
+        this.accountExpired = accountExpired;
+    }
+
+    /**
+     * @return if the account is non locked
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    /**
+     * @param accountLocked the accountLocked to set
+     */
+    public void setAccountLocked(Boolean accountLocked) {
+        this.accountLocked = accountLocked;
+    }
+
+    /**
+     * @return if the account is non expired
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !credentialsExpired;
+    }
+
+    /**
+     * @param credentialsExpired the credentialsExpired to set
+     */
+    public void setCredentialsExpired(Boolean credentialsExpired) {
+        this.credentialsExpired = credentialsExpired;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities=new LinkedHashSet<>();
+        authorities.addAll(getRoles());
+        return authorities;
+    }
+
+    /**
+     * @return the roles
+     */
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    /**
+     * @param roles the roles to set
+     */
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+    
+    public void addRole(Role role){
+        this.roles.add(role);
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the mothersMaidenName
+     */
+    public String getMothersMaidenName() {
+        return mothersMaidenName;
+    }
+
+    /**
+     * @param mothersMaidenName the mothersMaidenName to set
+     */
+    public void setMothersMaidenName(String mothersMaidenName) {
+        this.mothersMaidenName = mothersMaidenName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Override
+    public int hashCode(){
+        int hash=7;
+        hash=11*hash+Objects.hashCode(this.username);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj==null){
+            return false;
+        }
+        
+        if(getClass()!=obj.getClass()){
+            return false;
+        }
+        
+        final User other=(User)obj;
+        return Objects.equals(this.username, other.username);
+    }
+    
+    @Override
+    public String toString(){
+        return "User{username="+username+",name="+name+",lastName="+lastName+"}";
+    }
+}
