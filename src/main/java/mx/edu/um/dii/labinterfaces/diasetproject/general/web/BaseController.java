4424a0514e3a1e5a2a6060f20a6a2e74fdbfee42
Eder Martinez Lap UM Interfaces <edermtz.04@gmail.com>
2016-07-07 11:17:25 -0500

    create base controller

---
+++ src/main/java/mx/edu/um/dii/labinterfaces/diasetproject/general/web/BaseController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.um.dii.labinterfaces.diasetproject.general.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author laboratoriointerface
+ */
+public abstract class BaseController {
+    protected final transient Logger log = LoggerFactory.getLogger(getClass());
+        
+}
