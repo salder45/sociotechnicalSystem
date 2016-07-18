47c13365152250a8698a0a36b8c8f6b3d4468b8d
Eder Martinez Lap UM Interfaces <edermtz.04@gmail.com>
2016-07-18 16:42:25 -0500

    Create relationships between User and Role

--- src/main/java/mx/edu/um/dii/labinterfaces/diasetproject/dao/impl/InicializaDaoHibernate.java
+++ src/main/java/mx/edu/um/dii/labinterfaces/diasetproject/dao/impl/InicializaDaoHibernate.java
@@ -5,6 +5,7 @@
  */
 package mx.edu.um.dii.labinterfaces.diasetproject.dao.impl;
 
+import java.util.HashSet;
 import mx.edu.um.dii.labinterfaces.diasetproject.config.Constants;
 import mx.edu.um.dii.labinterfaces.diasetproject.dao.BaseDao;
 import mx.edu.um.dii.labinterfaces.diasetproject.dao.InicializaDao;
@@ -22,50 +23,62 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Repository
 @Transactional
-public class InicializaDaoHibernate extends BaseDao implements InicializaDao{
+public class InicializaDaoHibernate extends BaseDao implements InicializaDao {
 
     @Autowired
     private UserDao userDao;
     @Autowired
     private RoleDao roleDao;
-    
+
     @Override
     public void inicializa(String username, String password) {
         log.debug("Init data......");
         //Create Roles
+        HashSet roles=new HashSet();
         //ADMIN
-         Role roleAdmin=roleDao.get(Constants.ROLE_ADMIN);
-         log.debug("Role={}",roleAdmin);
-         if(roleAdmin==null||roleAdmin.getAuthority()==null||roleAdmin.getAuthority().equals("")){
-             roleAdmin=new Role(Constants.ROLE_ADMIN);
-             roleDao.save(roleAdmin);
-         }
-         //USER
-         Role roleUser=roleDao.get(Constants.ROLE_USER);
-         log.debug("Role={}",roleUser);
-         if(roleUser==null||roleUser.getAuthority()==null||roleUser.getAuthority().equals("")){
-             roleUser=new Role(Constants.ROLE_USER);
-             roleDao.save(roleUser);
-         }
+        Role roleAdmin = roleDao.get(Constants.ROLE_ADMIN);
+        log.debug("Role={}", roleAdmin);
+        if (roleAdmin == null || roleAdmin.getAuthority() == null || roleAdmin.getAuthority().equals("")) {
+            roleAdmin = new Role(Constants.ROLE_ADMIN);
+            roleDao.save(roleAdmin);
+        }
+        //USER
+        Role roleUser = roleDao.get(Constants.ROLE_USER);
+        log.debug("Role={}", roleUser);
+        if (roleUser == null || roleUser.getAuthority() == null || roleUser.getAuthority().equals("")) {
+            roleUser = new Role(Constants.ROLE_USER);
+            roleDao.save(roleUser);
+        }
         //Create admin/user and custom user
-        User userAdmin=userDao.get(Constants.DEFAULT_USER_ADMIN);
-        if(userAdmin==null||userAdmin.getId()==null||userAdmin.getId().equals(0L)){
-            userAdmin=new User(Constants.DEFAULT_USER_ADMIN, "admin", "admin", "admin", "admin");
+        User userAdmin = userDao.get(Constants.DEFAULT_USER_ADMIN);
+        if (userAdmin == null || userAdmin.getId() == null || userAdmin.getId().equals(0L)) {
+            userAdmin = new User(Constants.DEFAULT_USER_ADMIN, "admin", "admin", "admin", "admin");
+            userAdmin.addRole(roleDao.get(Constants.ROLE_ADMIN));
             //TODO check encodepassword option
             userDao.save(userAdmin);
         }
-        
-         User user=userDao.get(Constants.DEFAULT_USER);
-        if(user==null||userAdmin.getId()==null||userAdmin.getId().equals(0L)){
-            user=new User(Constants.DEFAULT_USER, "user", "user", "user", "user");
+
+        User user = userDao.get(Constants.DEFAULT_USER);
+        if (user == null || user.getId() == null || user.getId().equals(0L)) {
+            user = new User(Constants.DEFAULT_USER, "user", "user", "user", "user");
+            user.addRole(roleDao.get(Constants.ROLE_USER));
             //TODO check encodepassword option
             userDao.save(user);
         }
+
+        User customUser = userDao.get(username);
+        if (customUser == null || customUser.getId() == null || customUser.getId().equals(0L)) {
+            customUser = new User(username, password, "customUser", "customUser", "customUser");
+            customUser.addRole(roleDao.get(Constants.ROLE_USER));
+            //TODO check encodepassword option
+            userDao.save(customUser);
+        }
+
     }
 
     @Override
     public void inicializaRoles(String username, String password, String roles) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
