8ba8a9885babd0ca4bafc614da0d332e414df73f
Eder Martinez Lap UM Interfaces <edermtz.04@gmail.com>
2016-09-01 17:15:15 -0500

    Add area and machine model

---
+++ src/main/java/mx/edu/um/dii/labinterfaces/diasetproject/model/Machine.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.um.dii.labinterfaces.diasetproject.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ *
+ * @author laboratoriointerface
+ */
+@Entity
+@Table(name = "machines")
+public class Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Version
+    private Integer version;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "date_created",nullable = false)
+    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm zzz")
+    private Date dateCreated;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_updated",nullable = false)
+    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm zzz")
+    private Date lastUpdated;
+    @Column(nullable = false)
+    private String status;
+    private String code;
+    private String name;
+    private String description;
+    private String localization;
+    //Missing Area and result
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "area_id")
+    private Area area;
+}
