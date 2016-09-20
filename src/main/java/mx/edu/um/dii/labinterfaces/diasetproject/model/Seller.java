ec2de3fd441d7bb5dc477728d5a38d34dad2a9b7
Eder Martinez Lap UM Interfaces <edermtz.04@gmail.com>
2016-09-20 12:50:49 -0500

    create seller model

---
+++ src/main/java/mx/edu/um/dii/labinterfaces/diasetproject/model/Seller.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.um.dii.labinterfaces.diasetproject.model;
+
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ *
+ * @author laboratoriointerface
+ */
+@Entity
+@Table(name = "sellers")
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Version
+    private Integer version;
+     @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "date_created", nullable = false)
+    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm zzz")
+    private Date dateCreated;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_updated", nullable = false)
+    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm zzz")
+    private Date lastUpdated;
+    @Column(nullable = false)
+    private String status;
+    private String name;
+    /*
+    TODO addWorkOrders
+    */
+
+    /**
+     * @return the Id
+     */
+    public Long getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return the version
+     */
+    public Integer getVersion() {
+        return version;
+    }
+
+    /**
+     * @param version the version to set
+     */
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    /**
+     * @return the dateCreated
+     */
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    /**
+     * @param dateCreated the dateCreated to set
+     */
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    /**
+     * @return the lastUpdated
+     */
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    /**
+     * @param lastUpdated the lastUpdated to set
+     */
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public int hashCode() {
+     int hash = 7;
+        hash = 11 * hash + Objects.hash(this.Id, this.name);        
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        final Seller other=(Seller)obj;
+        return Objects.equals(this.Id, other.Id)&&Objects.equals(this.name, other.name);
+    }
+    
+    @Override
+    public String toString() {
+        return "{Seller{Id:"+this.Id+", Name:"+this.name+"}}";
+    }    
+}
