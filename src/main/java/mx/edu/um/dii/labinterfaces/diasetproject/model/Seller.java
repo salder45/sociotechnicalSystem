ab3412b4a918d429b83dd29e8553e139ab6883b3
Eder Martinez Lap UM Interfaces <edermtz.04@gmail.com>
2016-09-21 11:54:52 -0500

    Add serializable implements to Seller

--- src/main/java/mx/edu/um/dii/labinterfaces/diasetproject/model/Seller.java
+++ src/main/java/mx/edu/um/dii/labinterfaces/diasetproject/model/Seller.java
@@ -5,6 +5,7 @@
  */
 package mx.edu.um.dii.labinterfaces.diasetproject.model;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 import javax.persistence.Column;
@@ -24,7 +25,7 @@ import org.springframework.format.annotation.DateTimeFormat;
  */
 @Entity
 @Table(name = "sellers")
-public class Seller {
+public class Seller implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
